0bed4d2b71ca2f4f4ec38cc6a7bb8b2bd0a5b47a
Sayan Saha <sayan@sourcewiz.co>
2023-10-27 22:21:11 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+import Feed from '@components/Feed'
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptia is an open source tool for modern world to discover, create and
+        share creative prompts
+      </p>
+      <Feed/>
+    </section>
+  );
+};
+
+export default Home;
